b3b4feb460ed1046ff7edc72558578534e8e5253
Nick Bernard <nick@nrbernard.com>
2016-09-13 09:57:43 -0700

    Add link to new trip page

--- imports/ui/pages/home.jsx
+++ imports/ui/pages/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export const Home = () => (
   <div className="jumbotron">
     <h1>PackList</h1>
     <p>All the stuff that's fit to pack!</p>
-    <a href="#" className="btn btn-primary btn-lg">Get the App</a>
+    <Link to="/trips/new" className="btn btn-primary btn-lg">Plan Your Trip</Link>
   </div>
 );
