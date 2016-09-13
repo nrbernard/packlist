2bc95e9b8cc51beaf737d8d447ca697f48cd872b
Nick Bernard <nick@nrbernard.com>
2016-09-13 01:09:29 -0700

    Add placeholder homepage

--- client/main.jsx
+++ client/main.jsx
@@ -4,20 +4,19 @@ import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import '../imports/startup/accounts-config.js';
+
 import App from '../imports/ui/App.jsx';
-import ListsDisplay from '../imports/ui/ListsDisplay.jsx';
-import List from '../imports/ui/List.jsx';
 import NewTrip from '../imports/ui/NewTrip.jsx';
 import Trip from '../imports/ui/Trip.jsx';
 
+import { Home } from '../imports/ui/pages/home.jsx';
 import { NotFound } from '../imports/ui/pages/not-found.jsx';
 
 Meteor.startup(() => {
   render(
     <Router history={ browserHistory }>
       <Route path="/" component={ App }>
-        <IndexRoute component={ ListsDisplay } />
-        <Route path="/lists/:listId" component={ List } />
+        <IndexRoute component={ Home } />
         <Route path="/trips/new" component={ NewTrip } />
         <Route path="/trips/:tripId" component={ Trip } />
       </Route>
---
+++ imports/ui/pages/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Home = () => (
+  <div className="jumbotron">
+    <h1>PackList</h1>
+    <p>All the stuff that's fit to pack!</p>
+    <a href="#" className="btn btn-primary btn-lg">Get the App</a>
+  </div>
+);
